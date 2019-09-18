4d7a3bcc3e9ce2c540df33c0158dad116e28887d
tangentmonger <tangentmonger@users.noreply.github.com>
2019-09-18 23:46:53 +0100

    Remove unused code and tidy up

--- src/lightboots.ino
+++ src/lightboots.ino
@@ -15,10 +15,11 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 #define NUM_LEDS 16
 CRGB leds[NUM_LEDS];
 
+// todo: there are bugs that assume temperature is always 0-100
 #define MAX_TEMPERATURE 100
-double min_temperature = 0;
-double temperature = 100;
-double ambient = 0;
+#define MIN_TEMPERATURE 0
+double temperature = MAX_TEMPERATURE;
+double ambient = MIN_TEMPERATURE;
 double k = 0.5; // rate of cooling, higher is faster 
 
 #define RED_THRESHOLD 25
@@ -33,97 +34,99 @@ double k = 0.5; // rate of cooling, higher is faster
 
 void setup(void)
 {
-  Serial.begin(115200);
-  FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS);
-  for(int i=0;i<NUM_LEDS;i++)
-  {
-    leds[i].setRGB(0,127,0);
-  }
-  FastLED.show();
-  delay(300);
-  for(int i=0;i<NUM_LEDS;i++)
-  {
-    leds[i].setRGB(0,0,0);
-  }
-  FastLED.show();
-  
-  /* Initialise the sensor */
-  if (!bno.begin())
-  {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while (1);
-  }
+    Serial.begin(115200);
+    FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS);
+
+    flash_green();
+
+    /* Initialise the sensor */
+    if (!bno.begin())
+    {
+        /* There was a problem detecting the BNO055 ... check your connections */
+        Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+        while (1);
+    }
 }
 
 void loop(void)
 {
-  sensors_event_t linearAccelData;
-  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+    sensors_event_t linearAccelData;
+    bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
 
-  int total_data = abs(linearAccelData.acceleration.x) + abs(linearAccelData.acceleration.y) + abs(linearAccelData.acceleration.x);
-  Serial.print(total_data);
-  Serial.print(": ");
+    // just add up all the linear acceleration we're seeing
+    int total_data = abs(linearAccelData.acceleration.x) + abs(linearAccelData.acceleration.y) + abs(linearAccelData.acceleration.x);
+    Serial.print(total_data);
+    Serial.print(": ");
 
-  // do some cooling
-  temperature = ambient + (temperature * exp( -1 * k * BNO055_SAMPLERATE_DELAY_MS / 1000));
+    // do some cooling
+    temperature = ambient + (temperature * exp( -1 * k * BNO055_SAMPLERATE_DELAY_MS / 1000));
 
-  // do some heating
-  temperature += int(total_data * SENSITIVITY);
+    // do some heating
+    temperature += int(total_data * SENSITIVITY);
 
-  // apply thresholds
-  temperature = max(0, min(temperature, MAX_TEMPERATURE));
+    // apply thresholds
+    temperature = max(MIN_TEMPERATURE, min(temperature, MAX_TEMPERATURE));
 
-  Serial.print(temperature);
-  Serial.print(": ");
-  double hottest_point = temperature * HOTNESS_RATIO;
-  //Serial.print(hottest_point);
+    Serial.print(temperature);
+    Serial.print(": ");
 
-  double flame_height = double(temperature);
+    // "hottest point" depends on temperature, it's higher as it's hotter
+    double hottest_point = temperature * HOTNESS_RATIO;
 
-  for(int i=0; i<NUM_LEDS; i++){
-    // +1 otherwise the lowest LED is always off
-    double flame_part = double(i+1) / double(NUM_LEDS+1) * 100.0;
-    double led_temperature;
+    double flame_height = double(temperature);
 
-    if (flame_part < hottest_point)
-    {
-        led_temperature = flame_part / hottest_point * temperature;
-        //Serial.print(" ");
-        //Serial.print(led_temperature);
+    for(int i=0; i<NUM_LEDS; i++){
+        // calculate the temperature of this point in the flame
+        // +1 otherwise the lowest LED is always off
+        double flame_part = double(i+1) / double(NUM_LEDS+1) * 100.0;
+        double led_temperature;
 
-        //leds[i].setRGB(red_value(led_temperature),green_value(led_temperature),0);
+        if (flame_part < hottest_point)
+        {
+            led_temperature = flame_part / hottest_point * temperature;
+        }
+        else
+        {
+            led_temperature = temperature - ((flame_part - hottest_point) / (flame_height - hottest_point) * temperature);
+        }
 
+        led_temperature = max(MIN_TEMPERATURE, min(led_temperature, MAX_TEMPERATURE));
+
+        // colour the LED based on the temperature of this point in the flame
+        leds[i].setRGB(int(red_value(led_temperature) * BRIGHTNESS),
+                int(green_value(led_temperature) * BRIGHTNESS),
+                int(blue_value(led_temperature) * BRIGHTNESS));
+
+        Serial.print(" ");
+        Serial.print(blue_value(led_temperature));
     }
-    else
-    {
-        led_temperature = max(temperature - ((flame_part - hottest_point) / (flame_height - hottest_point) * temperature), 0);
 
-        //leds[i].setRGB(led_temperature,0,0);
+    Serial.println();
 
+    FastLED.show();
 
-    }
-   
-    leds[i].setRGB(int(red_value(led_temperature) * BRIGHTNESS),
-                   int(green_value(led_temperature) * BRIGHTNESS),
-                   int(blue_value(led_temperature) * BRIGHTNESS));
-
-    Serial.print(" ");
-    //Serial.print(led_temperature);
-
-    Serial.print(blue_value(led_temperature));
-  }
-  Serial.println();
-
-  FastLED.show();
-
-  delay(BNO055_SAMPLERATE_DELAY_MS);
+    delay(BNO055_SAMPLERATE_DELAY_MS);
 }
 
 
+void flash_green()
+{
+    for(int i=0;i<NUM_LEDS;i++)
+    {
+        leds[i].setRGB(0,127,0);
+    }
+    FastLED.show();
+    delay(300);
+
+    for(int i=0;i<NUM_LEDS;i++)
+    {
+        leds[i].setRGB(0,0,0);
+    }
+    FastLED.show();
+}
+
 int red_value(double temperature)
 {
-    //return 255;
     if (temperature > RED_THRESHOLD) return 255;
     return int(temperature / RED_THRESHOLD * 255);
 }
@@ -138,33 +141,6 @@ int green_value(double temperature)
 int blue_value(double temperature)
 {
     if (temperature < YELLOW_THRESHOLD) return 0;
-    return (temperature - YELLOW_THRESHOLD) / (MAX_TEMPERATURE - YELLOW_THRESHOLD) * 255;
+    return (temperature - YELLOW_THRESHOLD) / (WHITE_THRESHOLD - YELLOW_THRESHOLD) * 255;
 }
 
-
-double red_percentage(double temperature)
-{
-    if (temperature > RED_THRESHOLD) return 100;
-    return temperature / RED_THRESHOLD * 100;
-}
-
-double orange_percentage(double temperature)
-{
-    if (temperature < RED_THRESHOLD) return 0;
-    if (temperature > ORANGE_THRESHOLD) return 100;
-    return (temperature - RED_THRESHOLD) / (ORANGE_THRESHOLD - RED_THRESHOLD) * 100;
-}
-
-double yellow_percentage(double temperature)
-{
-    if (temperature < ORANGE_THRESHOLD) return 0;
-    if (temperature > YELLOW_THRESHOLD) return 100;
-    return (temperature - ORANGE_THRESHOLD) / (YELLOW_THRESHOLD - ORANGE_THRESHOLD) * 100;
-}
-
-double white_percentage(double temperature)
-{
-    if (temperature < YELLOW_THRESHOLD) return 0;
-    if (temperature > WHITE_THRESHOLD) return 100;
-    return (temperature - YELLOW_THRESHOLD) / (WHITE_THRESHOLD - YELLOW_THRESHOLD) * 100;
-}
